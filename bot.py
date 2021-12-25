6df4a646aa50bbf13b3cbf6663965f2551f4050d
sage228 <87944907+sage228@users.noreply.github.com>
2021-12-25 13:56:05 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,6 +3,9 @@ from config import bot_token
 import asyncio
 from aiogram import Bot, Dispatcher, executor, types
 
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get('GOOGLE_CHROME_SHIM', None)
+driver = webdriver.Chrome(executable_path="chromedriver", chrome_options=chrome_options)
 
 # Инициализируем бота
 loop = asyncio.get_event_loop()
@@ -13,10 +16,9 @@ dp = Dispatcher(bot, loop=loop)
 async def echo(message: types.Message):
     await message.answer(message.text)
     print(message.text)
-    # driver = webdriver.Chrome('C:\нужное\Python\BOT 2.0\chromedriver.exe')
-    # driver.get('https://sgo.rso23.ru')
-    # await message.answer(driver.find_element_by_xpath(
-    #     '/ html/body/div[2]/div[1]/div/div/span').text)
+    driver.get('https://sgo.rso23.ru')
+    await message.answer(driver.find_element_by_xpath(
+        '/ html/body/div[2]/div[1]/div/div/span').text)
 # запускаем лонг полинг
 if __name__ == '__main__':
     from handlers import dp, send_to_admin
